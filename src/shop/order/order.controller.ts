607bfa42348dcbdf4ba8d26c7694c5fbb053d774
Anik Khoma <anik123wb@gmail.com>
2023-06-07 23:21:13 +0300

    create order method, tests

--- src/shop/order/order.controller.ts
+++ src/shop/order/order.controller.ts
@@ -17,6 +17,7 @@ import {
 import { LanguageEnum } from '../../../shop_shared/constants/localization';
 import { OrderDto } from '../../../shop_shared/dto/order/order.dto';
 import { mapOrderDocumentToOrderDto } from '../../../shop_shared_server/mapper/order/map.orderDocument-to-orderDto';
+import { PublicError } from '../../../shop_shared_server/helpers/publicError';
 
 @Controller('order')
 export class OrderController {
@@ -58,30 +59,74 @@ export class OrderController {
     @Query('building') building: string,
     @Query('room') room: string,
   ): Promise<{ url: string }> {
-    const order = await this.orderService.createOrder({
-      firstName: first_name,
-      lastName: last_name,
-      phoneNumber: phone_number,
-      itemsData: JSON.parse(items_data),
-      delivery: {
-        whereToDeliver: where_to_deliver as NovaPoshtaDeliveryType,
-        data:
-          where_to_deliver === NOVA_POSHTA_DELIVERY_TYPE.OFFICE
-            ? {
-                cityName: city_name,
-                officeName: office_name,
-              }
-            : {
-                cityName: city_name,
-                street: street,
-                building: building,
-                room: room,
-              },
-      },
-    });
+    try {
+      if (!first_name) {
+        throw new PublicError('NO_FIRST_NAME');
+      }
+      if (!last_name) {
+        throw new PublicError('NO_LAST_NAME');
+      }
+      if (!phone_number) {
+        throw new PublicError('NO_PHONE_NUMBER');
+      }
+      if (!items_data) {
+        throw new PublicError('NO_ITEMS');
+      }
+      if (!where_to_deliver) {
+        throw new PublicError('NO_WHERE_TO_DELIVER');
+      }
+      if (!city_name) {
+        throw new PublicError('NO_CITY_NAME');
+      }
+      if (
+        !office_name &&
+        where_to_deliver === NOVA_POSHTA_DELIVERY_TYPE.OFFICE
+      ) {
+        throw new PublicError('NO_OFFICE_NAME');
+      }
+      if (!street && where_to_deliver === NOVA_POSHTA_DELIVERY_TYPE.COURIER) {
+        throw new PublicError('NO_STREET');
+      }
+      if (!building && where_to_deliver === NOVA_POSHTA_DELIVERY_TYPE.COURIER) {
+        throw new PublicError('NO_BUILDING');
+      }
+      if (!room && where_to_deliver === NOVA_POSHTA_DELIVERY_TYPE.COURIER) {
+        throw new PublicError('NO_ROOM');
+      }
 
-    return {
-      url: `http://localhost:3000/${lang}/order/${order._id.toString()}`,
-    };
+      const order = await this.orderService.createOrder({
+        firstName: first_name,
+        lastName: last_name,
+        phoneNumber: phone_number,
+        itemsData: JSON.parse(items_data),
+        delivery: {
+          whereToDeliver: where_to_deliver as NovaPoshtaDeliveryType,
+          data:
+            where_to_deliver === NOVA_POSHTA_DELIVERY_TYPE.OFFICE
+              ? {
+                  cityName: city_name,
+                  officeName: office_name,
+                }
+              : {
+                  cityName: city_name,
+                  street: street,
+                  building: building,
+                  room: room,
+                },
+        },
+      });
+
+      return {
+        url: `http://localhost:3000/${lang}/order/${order._id.toString()}`,
+      };
+    } catch (error) {
+      let errorDescription = `INTERNAL_ERROR`;
+      if (error instanceof PublicError) {
+        errorDescription = error.message;
+      }
+      return {
+        url: `http://localhost:3000/${lang}/order/failed_to_create?reason=${errorDescription}`,
+      };
+    }
   }
 }
