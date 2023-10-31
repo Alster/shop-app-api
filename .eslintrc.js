73077756d0e155dbefdb11d6e041616d5899251a
anikkhoma <anik123wb@gmail.com>
2023-11-01 00:49:23 +0200

    shared eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,8 @@
 module.exports = {
-	extends: "./shop-shared/.eslintrc.js",
+	extends: "./shop-shared/.eslintrc.nest.js",
+	parserOptions: {
+		project: "tsconfig.json",
+		tsconfigRootDir: __dirname,
+		sourceType: "module",
+	},
 };
