3cb47920cc9d49d648dde84d4cfb523010379bf5
anikkhoma <anik123wb@gmail.com>
2023-12-03 19:32:02 +0200

    :sparkles: updates

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,19 @@
+// eslint-disable-next-line unicorn/prefer-module
 module.exports = {
-	extends: "./shop-shared/.eslintrc.nest.js",
+	root: true,
 	parserOptions: {
+		ecmaVersion: "latest",
 		project: "tsconfig.json",
+		// eslint-disable-next-line unicorn/prefer-module
 		tsconfigRootDir: __dirname,
 		sourceType: "module",
+		parser: "@typescript-eslint/parser",
 	},
+	plugins: ["@typescript-eslint", "@typescript-eslint/eslint-plugin"],
+	env: {
+		es2021: true,
+		node: true,
+		jest: true,
+	},
+	extends: ["./shop-shared/.eslintrc.base.js", "./shop-shared/.eslintrc.nest.js"],
 };
