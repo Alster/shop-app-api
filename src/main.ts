41e95961eaaa75ac90a3289e07f72db9af7bc108
anikkhoma <anik123wb@gmail.com>
2024-06-25 10:51:13 +0300

    :sparkles: updates

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 require("dotenv").config();
 
-import { ValidationPipe } from "@nestjs/common";
+import { Logger, ValidationPipe } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
 import { setupGracefulShutdown } from "nestjs-graceful-shutdown";
 
 import { AppModule } from "./app.module";
 import { Config } from "./config/config";
+
+const logger = new Logger("Main");
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
@@ -20,6 +22,9 @@ async function bootstrap() {
 	if (Config.env === "local") {
 		app.enableCors();
 	}
-	await app.listen(Config.get().port);
+	await app.listen(Config.get().port, "0.0.0.0");
+	logger.log(`Application is running on: ${await app.getUrl()}`);
 }
-bootstrap();
+
+// eslint-disable-next-line unicorn/prefer-top-level-await
+void bootstrap();
